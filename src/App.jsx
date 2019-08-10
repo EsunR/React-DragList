7443456add472fd327e09fb155b70524e514251f
EsunR <641411169@qq.com>
2019-08-10 19:00:47 +0800

    实现基本功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import DragList from './components/list';
+import './App.scss';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			data: ['数据1', '数据2', '数据3', '数据4']
+			data: [
+				{
+					data: "数据1",
+					id: 1
+				},
+				{
+					data: "数据2",
+					id: 2
+				},
+				{
+					data: "数据3",
+					id: 3
+				},
+				{
+					data: "数据4",
+					id: 4
+				},
+				{
+					data: "数据5",
+					id: 5
+				},
+				{
+					data: "数据6",
+					id: 6
+				}
+			]
 		}
+	}
+
+	handleSorted(sortedData) {
+		console.log(sortedData);
 	}
 
 	render() {
@@ -15,8 +45,13 @@ class App extends Component {
 				<h3>这是一个拖拽列表demo</h3>
 				<DragList
 					data={this.state.data}
-					style={{ width: "300px" }}
+					style={{
+						width: "300px",
+						border: "5px solid skyblue",
+						padding: "20px"
+					}}
 					spacing={20}
+					onSorted={this.handleSorted}
 				/>
 			</div >
 		);
--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -5,12 +5,12 @@ export class DragListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      moving: false,
       top: 0,
       indexStep: 0,
       throwDomList: []
     }
     this.itemDom = React.createRef();
-    this.moving = false;
     this.startY = 0;
     this.originTop = 0;
     this.zIndex = 1;
@@ -21,7 +21,7 @@ export class DragListItem extends Component {
   componentDidUpdate(prevProps, prevState) {
     // 如果检测出经过了一个div就开始变动 trowDomList
     if (this.state.indexStep !== prevState.indexStep) {
-      let throwingIndex = this.props.startIndex + this.state.indexStep; // 经过的dom索引
+      let throwingIndex = this.nextIndex; // 经过的dom索引
       let throwDomList = this.state.throwDomList
 
       // 如果经过的位置不是自己原来的位置
@@ -29,19 +29,25 @@ export class DragListItem extends Component {
         let throwDom = this.itemDom.current.parentNode.children[throwingIndex];
         let inArray = throwDomList.some((item, index) => {
           if (item.innerHTML === throwDom.innerHTML) {
-            throwDomList.splice(index + 1, 1)
+            throwDomList.splice(index + 1, 1)[0].classList.remove("move-up", "move-down");
             this.setState({ throwDomList });
             return true;
+          } else {
+            return false;
           }
         })
         if (!inArray) {
           throwDomList.push(throwDom);
+          this.setState({ throwDomList });
           let className = this.state.indexStep > 0 ? "move-up" : "move-down";
           throwDom.classList.add(className);
-          this.setState({ throwDomList });
         }
       } else {
-        this.setState({ throwDomList: [] });
+        // 当元素被移出边界时，组件会被强制终止排序，所有组件重新渲染 throwDomList 会被重置，这时再去清除其上挂载的 class 会报错
+        if (this.state.throwDomList[0]) {
+          this.state.throwDomList[0].classList.remove("move-up", "move-down");
+          this.setState({ throwDomList: [] });
+        }
       }
     }
   }
@@ -56,28 +62,35 @@ export class DragListItem extends Component {
   }
 
   handleMouseMove(e) {
-    if (this.moving) {
+    e.preventDefault();
+    if (this.state.moving) {
       // 缓存拖动距离
       let moveY = e.clientY - this.startY;
-
       // 当拖动距离距超过下一个元素的高度的一半时，移动距离 +1， 计算出下一个位置的索引值
       let indexStep = moveY > 0 ?
         Math.ceil((moveY - this.stepLength / 2) / this.stepLength) :
         Math.floor((moveY + this.stepLength / 2) / this.stepLength)
-      this.nextIndex = this.props.startIndex + indexStep;
-
-      // 修改视图
-      this.setState({
-        top: this.originTop + moveY,
-        indexStep
-      })
-    } else {
-      return;
+      let nextIndex = this.props.startIndex + indexStep;
+      let upEdge = this.props.parent.state.data.length - 1;
+      // 如果超过上下边界就强行结束拖拽
+      if (nextIndex > upEdge) {
+        this.handleDragOver();
+        this.nextIndex = upEdge;
+      } else if (nextIndex < 0) {
+        this.handleDragOver();
+        this.nextIndex = 0;
+      } else {
+        this.nextIndex = this.props.startIndex + indexStep;
+        this.setState({
+          top: this.originTop + moveY,
+          indexStep
+        })
+      }
     }
   }
 
   handleMouseDown(e) {
-    this.moving = true;
+    this.setState({ moving: true })
     this.zIndex = 999;
     this.startY = e.clientY;
     this.originTop = this._getDomStyleNum(this.itemDom.current, "top");
@@ -90,42 +103,47 @@ export class DragListItem extends Component {
     })
   }
 
-  handleDragOver(e) {
-    if (this.state.indexStep !== 0) {
-      this.props.parent.resort(this.props.startIndex, this.nextIndex);
+  handleDragOver() {
+    if (this.state.moving) {
+      if (this.state.indexStep !== 0) {
+        this.props.parent.resort(this.props.startIndex, this.nextIndex);
+      }
+      this.zIndex = 1;
+      this.startY = 0;
+      this.moveY = 0;
+      this.setState({
+        top: 0,
+        indexStep: 0,
+        moving: false
+      });
+      // 清除动画样式，让元素归位
+      let brotherDom = this.itemDom.current.parentNode.children
+      Array.prototype.forEach.call(brotherDom, (item) => {
+        item.classList.remove("move-up", "move-down", "animate");
+      })
+      this.props.parent.props.onSorted(this.props.parent.state.data);
     }
-    this.moving = false;
-    this.zIndex = 1;
-    this.startY = 0;
-    this.moveY = 0;
-    this.setState({
-      top: 0,
-      indexStep: 0
-    });
-    // 清除动画样式，让元素归位
-    let brotherDom = this.itemDom.current.parentNode.children
-    Array.prototype.forEach.call(brotherDom, (item) => {
-      item.classList.remove("move-up", "move-down", "animate");
-    })
   }
 
   render() {
     return (
-      <div className="drag-list-item" ref={this.itemDom} style={{
-        margin: `${this.props.spacing ? this.props.spacing : 0}px 0px`,
-        top: this.state.top,
-        zIndex: this.zIndex
-      }}>
+      <div
+        className="drag-list-item"
+        ref={this.itemDom}
+        style={{
+          margin: `${this.props.spacing ? this.props.spacing : 0}px 0px`,
+          top: this.state.top,
+          zIndex: this.zIndex
+        }}
+        onMouseDown={this.handleMouseDown.bind(this)}
+        onMouseUp={this.handleDragOver.bind(this)}
+        onMouseMove={this.handleMouseMove.bind(this)}
+        onMouseLeave={this.handleDragOver.bind(this)}
+      >
         <div className="item-content">
           {this.props.data}
         </div>
-        <div
-          className="drag-handle"
-          onMouseDown={this.handleMouseDown.bind(this)}
-          onMouseUp={this.handleDragOver.bind(this)}
-          onMouseMove={this.handleMouseMove.bind(this)}
-          onMouseLeave={this.handleDragOver.bind(this)}
-        ></div>
+        {/* <div className="drag-handle"></div> */}
       </div>
     )
   }
@@ -142,7 +160,8 @@ export default class DragList extends Component {
   }
 
   componentDidMount() {
-    this.setState({ data: this.props.data })
+    // 将原数据进行拷贝隔离
+    this.setState({ data: JSON.parse(JSON.stringify(this.props.data)) })
   }
 
   resort(prevIndex, nextIndex) {
@@ -154,8 +173,8 @@ export default class DragList extends Component {
   render() {
     let listItem = this.state.data.map((item, index) =>
       <DragListItem
-        key={item}
-        data={item}
+        key={item.id}
+        data={item.data}
         spacing={this.props.spacing}
         startIndex={index}
         parent={this}
@@ -163,11 +182,13 @@ export default class DragList extends Component {
     )
 
     return (
-      <div className="drag-list" style={{
-        // margin: `${this.props.spacing ? - this.props.spacing : 0}px 0px`,
-        ...this.props.style
-      }}>
-        {listItem}
+      <div className="drag-list" style={{ ...this.props.style }}>
+        <div
+          className="drag-list-inner"
+          style={{ margin: `${this.props.spacing ? - this.props.spacing : 0}px 0px`, }}
+        >
+          {listItem}
+        </div>
       </div>
     )
   }
