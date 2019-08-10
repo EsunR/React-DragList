7b8132e878c66ca46951390f94026c8ad6965819
EsunR <641411169@qq.com>
2019-08-10 17:20:25 +0800

    添加滑动动画

--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -5,16 +5,45 @@ export class DragListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      top: 0
+      top: 0,
+      indexStep: 0,
+      throwDomList: []
     }
     this.itemDom = React.createRef();
     this.moving = false;
     this.startY = 0;
-    this.indexStep = 0;
     this.originTop = 0;
     this.zIndex = 1;
     this.stepLength = null;
     this.nextIndex = null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // 如果检测出经过了一个div就开始变动 trowDomList
+    if (this.state.indexStep !== prevState.indexStep) {
+      let throwingIndex = this.props.startIndex + this.state.indexStep; // 经过的dom索引
+      let throwDomList = this.state.throwDomList
+
+      // 如果经过的位置不是自己原来的位置
+      if (throwingIndex !== this.props.startIndex) {
+        let throwDom = this.itemDom.current.parentNode.children[throwingIndex];
+        let inArray = throwDomList.some((item, index) => {
+          if (item.innerHTML === throwDom.innerHTML) {
+            throwDomList.splice(index + 1, 1)
+            this.setState({ throwDomList });
+            return true;
+          }
+        })
+        if (!inArray) {
+          throwDomList.push(throwDom);
+          let className = this.state.indexStep > 0 ? "move-up" : "move-down";
+          throwDom.classList.add(className);
+          this.setState({ throwDomList });
+        }
+      } else {
+        this.setState({ throwDomList: [] });
+      }
+    }
   }
 
   _getDomStyleNum(dom, style) {
@@ -32,14 +61,15 @@ export class DragListItem extends Component {
       let moveY = e.clientY - this.startY;
 
       // 当拖动距离距超过下一个元素的高度的一半时，移动距离 +1， 计算出下一个位置的索引值
-      this.indexStep = moveY > 0 ?
+      let indexStep = moveY > 0 ?
         Math.ceil((moveY - this.stepLength / 2) / this.stepLength) :
         Math.floor((moveY + this.stepLength / 2) / this.stepLength)
-      this.nextIndex = this.props.startIndex + this.indexStep;
+      this.nextIndex = this.props.startIndex + indexStep;
 
       // 修改视图
       this.setState({
-        top: this.originTop + moveY
+        top: this.originTop + moveY,
+        indexStep
       })
     } else {
       return;
@@ -51,18 +81,32 @@ export class DragListItem extends Component {
     this.zIndex = 999;
     this.startY = e.clientY;
     this.originTop = this._getDomStyleNum(this.itemDom.current, "top");
+    // 除了自己，给兄弟节点全部套上 transition 效果
+    let brotherDom = this.itemDom.current.parentNode.children
+    Array.prototype.forEach.call(brotherDom, (item, index) => {
+      if (index !== this.props.startIndex) {
+        item.classList.add("animate");
+      }
+    })
   }
 
   handleDragOver(e) {
-    if (this.indexStep !== 0) {
+    if (this.state.indexStep !== 0) {
       this.props.parent.resort(this.props.startIndex, this.nextIndex);
     }
     this.moving = false;
     this.zIndex = 1;
     this.startY = 0;
     this.moveY = 0;
-    this.indexStep = 0;
-    this.setState({ top: 0 });
+    this.setState({
+      top: 0,
+      indexStep: 0
+    });
+    // 清除动画样式，让元素归位
+    let brotherDom = this.itemDom.current.parentNode.children
+    Array.prototype.forEach.call(brotherDom, (item) => {
+      item.classList.remove("move-up", "move-down", "animate");
+    })
   }
 
   render() {
